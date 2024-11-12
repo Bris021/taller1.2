a201c38350f25929c6dfc6c004ad14e38837df39
Briss <dennis.chuqui2003@gmail.com>
2024-11-11 23:27:54 -0500

    .

--- Sisven/ventas/models.py
+++ Sisven/ventas/models.py
@@ -6,13 +6,13 @@ class Categoria(models.Model):
     def __str__(self):
         return self.nombre
 class Producto(models.Model):
-        nombre= models.CharField(max_length=100,verbose_name= "Nombre", unique=True,  blank=False)
-        precio = models.DecimalField(max_digits=4, decimal_places=2, verbose_name= "precio", blank= False)
-        stock = models.IntegerField()
-        categoria = models.ForeignKey(Categoria, on_delete=models.RESTRICT )
+    nombre = models.CharField(max_length=100, blank=True)
+    descripcion = models.TextField(max_length=2000, blank=True)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
 
-        def __str__(self):
-            return self.nombre
+    def __str__(self):
+        return self.nombre
         
 class Cliente (models.Model):
             nombre_cliente= models.CharField(max_length=50, verbose_name= "Nombre", unique=True, blank=False)
